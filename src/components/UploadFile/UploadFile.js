290387b3d144ac7964e4e91e7d944305e97e0672
Alexander Treib <alextreib94@gmail.com>
2020-11-21 11:05:13 +0100

    formatting and uploadfile

--- src/components/UploadFile/UploadFile.js
+++ src/components/UploadFile/UploadFile.js
@@ -1,65 +1,106 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Button from "components/CustomButtons/Button.js";
 
 import firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/firestore";
+import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from '@material-ui/core/styles';
 
+
+// const { FloatingActionButton, SvgIcon, MuiThemeProvider, getMuiTheme } = MaterialUI;
 import IconButton from "@material-ui/core/IconButton";
 import Icon from "@material-ui/core/Icon";
+import AddIcon from "@material-ui/icons/Add";
+
+import Fab from "@material-ui/core/Fab";
+
+import PropTypes from "prop-types";
+
+const styles = (theme) => ({
+  // This group of buttons will be aligned to the right
+  rightToolbar: {
+    marginLeft: "auto",
+  },
+  menuButton: {
+    marginRight: 16,
+    marginLeft: -12,
+  },
+});
 
 class FileDialogue extends React.Component {
-    constructor(props) { // highlight-range{3}
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.fileInput = React.createRef();
-    }
-    handleSubmit(event) { // highlight-range{3}
-        event.preventDefault();
-        console.log(this.fileInput.current.files);
+  constructor(props) {
+    const script = document.createElement("script");
 
-        alert(`Selected file - ${
-            this.fileInput.current.files[0].name
-        }`);
+    script.src =
+      "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js";
+    script.async = true;
 
-        // Create a root reference
-        var storageRef = firebase.storage().ref();
+    document.body.appendChild(script);
+    // highlight-range{3}
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.fileInput = React.createRef();
+  }
 
-        var file = this.fileInput.current.files[0];
-        var uploadTask = storageRef.child('testimage.png').put(file).then(function (snapshot) {
-            console.log('Uploaded a blob or file!');
-        });
+  onChangeHandler = (event) => {
+    console.log("fired");
+    console.log(event.target.files[0]);
+  };
 
-        console.log(uploadTask);
-    }
+  handleSubmit(event) {
+    // highlight-range{3}
+    event.preventDefault();
 
-    render() {
-        return (
-            <form onSubmit={
-                this.handleSubmit
-            }>
+    var fileToUpload=event.target.files[0];
+    var fileName=fileToUpload.name;
 
-                <label>
-                    Upload file:
-                    <input type="file"
-                        ref={
-                            this.fileInput
-                        }/>
-                </label>
-                <br/>
-                <IconButton type="submit"
-                    style={
-                        {
-                            marginRight: "4vw",
-                            alignSelf: "flex-end"
-                        }
-                }>
-                    <Icon fontSize="large" className="fa fa-plus-circle" color="primary"/>
-                </IconButton>
-            </form>
-        );
-    }
+    console.log(fileToUpload);
+    alert(`Uploaded file - ${fileName}`);
+
+    // Create a root reference
+    var storageRef = firebase.storage().ref();
+
+    var uploadTask = storageRef
+      .child(fileName)
+      .put(fileToUpload)
+      .then(function (snapshot) {
+        console.log("Uploaded file!");
+      });
+
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <section className={classes.rightToolbar}>
+        <div>
+          <input
+            id="myInput"
+            type="file"
+            ref={(ref) => (this.myInput = ref)}
+            style={{ display: "none" }}
+            onChange={this.handleSubmit}
+          />
+
+          {/* Only button */}
+          <Fab
+            color="primary"
+            aria-label="add"
+            style={{ right: "0px" }}
+            onClick={(e) => this.myInput.click()}
+          >
+            <AddIcon />
+          </Fab>
+        </div>
+      </section>
+    );
+  }
 }
 
-export default FileDialogue;
+FileDialogue.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(FileDialogue);
--- src/views/ReportOverview/ReportOverview.js
+++ src/views/ReportOverview/ReportOverview.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // @material-ui/core components
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
@@ -14,8 +14,8 @@ import IconButton from "@material-ui/core/IconButton";
 import Icon from "@material-ui/core/Icon";
 
 import UploadFile from "components/UploadFile/UploadFile.js";
-import MaterialTable from 'material-table';
-
+import ShowFile from "components/ShowFile/ShowFile.js";
+import MaterialTable from "material-table";
 
 import AddAlert from "@material-ui/icons/AddAlert";
 import SnackbarContent from "components/Snackbar/SnackbarContent.js";
@@ -28,162 +28,137 @@ import "firebase/firestore";
 // import FileBrowser from "react-keyed-file-browser";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCpS3fCBYZcehmfwhs6ma_6uyhw6FKmYfM",
-    authDomain: "spexdoc.firebaseapp.com",
-    databaseURL: "https://spexdoc.firebaseio.com",
-    projectId: "spexdoc",
-    storageBucket: "spexdoc.appspot.com",
-    messagingSenderId: "890835351206",
-    appId: "1:890835351206:web:78e087ece687649ae8e667",
-    measurementId: "G-20H8X0HLQ9"
+  apiKey: "AIzaSyCpS3fCBYZcehmfwhs6ma_6uyhw6FKmYfM",
+  authDomain: "spexdoc.firebaseapp.com",
+  databaseURL: "https://spexdoc.firebaseio.com",
+  projectId: "spexdoc",
+  storageBucket: "spexdoc.appspot.com",
+  messagingSenderId: "890835351206",
+  appId: "1:890835351206:web:78e087ece687649ae8e667",
+  measurementId: "G-20H8X0HLQ9",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const styles = {
-    cardCategoryWhite: {
-        "&,& a,& a:hover,& a:focus": {
-            color: "rgba(255,255,255,.62)",
-            margin: "0",
-            fontSize: "14px",
-            marginTop: "0",
-            marginBottom: "0"
-        },
-        "& a,& a:hover,& a:focus": {
-            color: "#FFFFFF"
-        }
+  cardCategoryWhite: {
+    "&,& a,& a:hover,& a:focus": {
+      color: "rgba(255,255,255,.62)",
+      margin: "0",
+      fontSize: "14px",
+      marginTop: "0",
+      marginBottom: "0",
     },
-    cardTitleWhite: {
-        color: "#FFFFFF",
-        marginTop: "0px",
-        minHeight: "auto",
-        fontWeight: "300",
-        fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-        marginBottom: "3px",
-        textDecoration: "none",
-        "& small": {
-            color: "#777",
-            fontSize: "65%",
-            fontWeight: "400",
-            lineHeight: "1"
-        }
+    "& a,& a:hover,& a:focus": {
+      color: "#FFFFFF",
     },
-    ref: "https://google.de"
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1",
+    },
+  },
+  ref: "https://google.de",
 };
 
 const useStyles = makeStyles(styles);
 
 export default function ReportOverview() {
-    const [fileName, setFileName] = React.useState();
-    const [selectedFile, setSelectedFile] = React.useState();
+  const [fileName, setFileName] = React.useState();
+  const [selectedFile, setSelectedFile] = React.useState();
 
-    const handleFilePicker = async (e) => {
-        console.log("go");
-        let file = e.target.files[0];
-        // read data from the blob objects(file)
-        let reader = new FileReader();
-        // reads the binary data and encodes it as base64 data url
-        reader.readAsDataURL(file);
-        // reads it finish with either success or failure
-        reader.onloadend = () => { // reader.result is the result of the reading in base64 string
-            setFileName(file.name);
-            setSelectedFile(reader.result);
-        };
-        this.sendImageData();
+  const handleFilePicker = async (e) => {
+    console.log("go");
+    let file = e.target.files[0];
+    // read data from the blob objects(file)
+    let reader = new FileReader();
+    // reads the binary data and encodes it as base64 data url
+    reader.readAsDataURL(file);
+    // reads it finish with either success or failure
+    reader.onloadend = () => {
+      // reader.result is the result of the reading in base64 string
+      setFileName(file.name);
+      setSelectedFile(reader.result);
     };
+    this.sendImageData();
+  };
 
-    var mount = document.querySelectorAll("div.browser-mount");
+  var mount = document.querySelectorAll("div.browser-mount");
 
-    const classes = useStyles();
-    return (
-        <GridContainer>
-            <GridItem xs={12}
-                sm={12}
-                md={12}>
-                <Card>
-                    <CardHeader color="primary">
-                        <h4 className={
-                            classes.cardTitleWhite
-                        }>Dermatologie</h4>
-                        <p className={
-                            classes.cardCategoryWhite
-                        }>Untertitel</p>
-                    </CardHeader>
-                    <CardBody>
-                        <Table tableHeaderColor="primary"
-                            tableHead={
-                                ["ID", "Arzt", "Datum", "Ort"]
-                            }
-                            tableData={
-                                [
-                                    [
-                                        "213aer", "Dr. Wilder", "24.03.2020", "Stuttgart"
-                                    ],
-                                    [
-                                        "234aef", "Dr. med. Fechtele", "12.03.2020", "Berl ain"
-                                    ],
-                                ]
-                            }/>
-                    </CardBody>
-                    <IconButton style={
-                        {
-                            marginRight: "4vw",
-                            alignSelf: "flex-end"
-                        }
-                    }>
-                        <Icon fontSize="large" className="fa fa-plus-circle" color="primary"/>
-                    </IconButton>
-                </Card>
-            </GridItem>
-            <GridItem xs={12}
-                sm={12}
-                md={12}>
-                <Card>
-                    <CardHeader color="primary">
-                        <h4 className={
-                            classes.cardTitleWhite
-                        }>Allergologie</h4>
-                        <p className={
-                            classes.cardCategoryWhite
-                        }>Untertitel</p>
-                    </CardHeader>
-                    <CardBody>
-                        <Table tableHeaderColor="primary"
-                            tableHead={
-                                ["ID", "Arzt", "Datum", "Ort"]
-                            }
-                            tableData={
-                                [
-                                    [
-                                        "232efr", "Dr. Haut", "24.07.2020", "Offenburg"
-                                    ],
-                                    [
-                                        "asj2ef", "Dr. med. Hornung", "12.03.2020", "Freiburg"
-                                    ],
-                                ]
-                            }/>
-                    </CardBody>
-                </Card>
-
-            </GridItem>
-            <GridItem xs={12}
-                sm={12}
-                md={12}>
-                <Card>
-                    <CardHeader color="primary">
-                        <h4 className={
-                            classes.cardTitleWhite
-                        }>Allergologie</h4>
-                        <p className={
-                            classes.cardCategoryWhite
-                        }>Untertitel</p>
-                    </CardHeader>
-                    <CardBody>
-                        <EditableTableReport/>
-                    </CardBody>
-                </Card>
-            </GridItem>
-            <UploadFile/>
-        </GridContainer>
-    );
+  const classes = useStyles();
+  return (
+    <GridContainer>
+      <GridItem xs={12} sm={12} md={12}>
+        <Card>
+          <CardHeader color="primary">
+            <h4 className={classes.cardTitleWhite}>Dermatologie</h4>
+            <p className={classes.cardCategoryWhite}>Untertitel</p>
+          </CardHeader>
+          <CardBody>
+            <Table
+              tableHeaderColor="primary"
+              tableHead={["ID", "Arzt", "Datum", "Ort"]}
+              tableData={[
+                ["213aer", "Dr. Wilder", "24.03.2020", "Stuttgart"],
+                ["234aef", "Dr. med. Fechtele", "12.03.2020", "Berl ain"],
+              ]}
+            />
+          </CardBody>
+          <IconButton
+            style={{
+              marginRight: "4vw",
+              alignSelf: "flex-end",
+            }}
+          >
+            <Icon
+              fontSize="large"
+              className="fa fa-plus-circle"
+              color="primary"
+            />
+          </IconButton>
+        </Card>
+      </GridItem>
+      <GridItem xs={12} sm={12} md={12}>
+        <Card>
+          <CardHeader color="primary">
+            <h4 className={classes.cardTitleWhite}>Allergologie</h4>
+            <p className={classes.cardCategoryWhite}>Untertitel</p>
+          </CardHeader>
+          <CardBody>
+            <Table
+              tableHeaderColor="primary"
+              tableHead={["ID", "Arzt", "Datum", "Ort"]}
+              tableData={[
+                ["232efr", "Dr. Haut", "24.07.2020", "Offenburg"],
+                ["asj2ef", "Dr. med. Hornung", "12.03.2020", "Freiburg"],
+              ]}
+            />
+          </CardBody>
+        </Card>
+      </GridItem>
+      <GridItem xs={12} sm={12} md={12}>
+        <Card>
+          <CardHeader color="primary">
+            <h4 className={classes.cardTitleWhite}>Allergologie</h4>
+            <p className={classes.cardCategoryWhite}>Untertitel</p>
+          </CardHeader>
+          <CardBody>
+            <EditableTableReport />
+            <ShowFile />
+            <UploadFile />
+          </CardBody>
+        </Card>
+      </GridItem>
+    </GridContainer>
+  );
 }
