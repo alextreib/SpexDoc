f5c47175f7d7e732fabba7e50a4d457ec66a8c9d
Alexander Treib <alextreib94@gmail.com>
2020-11-29 22:42:49 +0100

    integrating installation with pwa, very nice feature + logo corrections

--- src/index.js
+++ src/index.js
@@ -27,6 +27,9 @@ import RTL from "layouts/RTL.js";
 import { Provider } from "react-redux";
 import { store } from "components/Internal/Redux.js";
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
 import "assets/css/material-dashboard-react.css?v=1.9.0";
 
 const hist = createBrowserHistory();
@@ -44,3 +47,14 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
