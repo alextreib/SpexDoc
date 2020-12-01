123d4a1547df90e5133f896d5621af698f31549e
Alexander Treib <alextreib94@gmail.com>
2020-12-02 00:54:52 +0100

    fixing bottomappbar

--- src/components/Navbars/BottomAppBarMobile.js
+++ src/components/Navbars/BottomAppBarMobile.js
@@ -6,9 +6,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Badge from "@material-ui/core/Badge";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import HomeIcon from "@material-ui/icons/Home";
-import IconButton from "@material-ui/core/IconButton";
 import { Link } from "react-router-dom";
-import { NavLink } from "react-router-dom";
 import NotificationData from "components/NotificationData/NotificationData.js";
 import NotificationIcon from "@material-ui/icons/Notifications";
 import ProfileButton from "components/Navbars/ProfileButton.js";
@@ -17,6 +15,11 @@ import Toolbar from "@material-ui/core/Toolbar";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > * + *": {
+      marginLeft: theme.spacing(5),
+    },
+  },
   text: {
     padding: theme.spacing(2, 2, 0),
   },
@@ -32,7 +35,7 @@ const useStyles = makeStyles((theme) => ({
   appBar: {
     top: "auto",
     bottom: 0,
-    backgroundColor: grayColor[10],//todo: maybe black?
+    backgroundColor: grayColor[10], //todo: maybe black?
     boxShadow: "none",
     borderBottom: "0",
     marginBottom: "0",
@@ -52,6 +55,18 @@ const useStyles = makeStyles((theme) => ({
     left: 0,
     right: 0,
     margin: "0 auto",
+  },
+  Home: {
+    color: "inherit",
+    marginLeft: theme.spacing(2),
+  },
+  LinkNotification: {
+    color: "inherit",
+    marginRight: theme.spacing(2),
+  },
+  Profile: {
+    color: "inherit",
+    marginRight: theme.spacing(5),
   },
 }));
 
@@ -89,30 +104,18 @@ export default function BottomAppBarMobile() {
     <React.Fragment>
       <CssBaseline />
       <NotificationData onNotificationDataChange={notificationDataChange} />
-      <AppBar className={classes.appBar}>
+      <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
-          <NavLink
-            to="/dashboard"
-            activeClassName="active"
-            style={{ textDecoration: "none" }}
-          >
-            {/* <Link style={{ textDecoration: "none" }} to="/dashboard"> */}
-            <IconButton edge="start" color="inherit" aria-label="open drawer">
+          <Link className={classes.Home} to="/dashboard">
               <HomeIcon />
-            </IconButton>
-          </NavLink>
+          </Link>
           <div className={classes.grow} />
-
-          {/* todo: adding paper/dropdown list with itemsm, also wrong color */}
-          <Link color="inherit" to="/notifications">
-            <Badge badgeContent={notificationList.length} color="primary">
-              <NotificationIcon
-                color="black"
-                onClick={handleClickNotification}
-              />
+          <Link className={classes.LinkNotification} to="/notifications">
+            <Badge badgeContent={notificationList.length} color="secondary">
+              <NotificationIcon />
             </Badge>
           </Link>
-          <ProfileButton />
+          <ProfileButton className={classes.Profile} />
         </Toolbar>
       </AppBar>
     </React.Fragment>
